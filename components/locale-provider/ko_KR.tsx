6619c85b34fffed8613ea7bbf80f323571f37813
Yang Taegyu <iamytg@gmail.com>
2020-07-10 10:08:34 +0900

    Create ko_KR.tsx

---
+++ components/locale-provider/ko_KR.tsx
@@ -0,0 +1,22 @@
+import DatePickerView from '../date-picker-view/locale/ko_KR';
+import DatePicker from '../date-picker/locale/ko_KR';
+import InputItem from '../input-item/locale/ko_KR';
+import Pagination from '../pagination/locale/ko_KR';
+import Picker from '../picker/locale/ko_KR';
+import SearchBar from '../search-bar/locale/ko_KR';
+import Menu from '../menu/locale/ko_KR';
+import PullToRefresh from '../pull-to-refresh/locale/ko_KR';
+import Calendar from '../calendar/locale/ko_KR';
+
+export default {
+  locale: 'ko',
+  Pagination,
+  DatePicker,
+  DatePickerView,
+  InputItem,
+  Picker,
+  SearchBar,
+  Menu,
+  PullToRefresh,
+  Calendar,
+};
